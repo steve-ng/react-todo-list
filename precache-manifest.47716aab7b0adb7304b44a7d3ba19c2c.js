bac61581a56ffd988df750ea5ba2133abf55cf47
steve-ng <steve-ng@users.noreply.github.com>
2020-07-26 11:42:44 +0800

    Updates

---
+++ precache-manifest.47716aab7b0adb7304b44a7d3ba19c2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d1e97a867a26fde64dea6df6a4c17d",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "e497e0349019d318588c",
+    "url": "/react-todo-list/static/css/main.fb119a93.chunk.css"
+  },
+  {
+    "revision": "d32f933c485f1e0833aa",
+    "url": "/react-todo-list/static/js/2.581cc814.chunk.js"
+  },
+  {
+    "revision": "e497e0349019d318588c",
+    "url": "/react-todo-list/static/js/main.266721f0.chunk.js"
+  },
+  {
+    "revision": "89737d6b5129f89a8a0b",
+    "url": "/react-todo-list/static/js/runtime~main.eb2f80b6.js"
+  }
+]);
